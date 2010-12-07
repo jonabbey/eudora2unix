9ad7ae26acb26eabd3cda21633305d5b30380ceb
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2010-12-07 14:37:39 -0600

    Truly fixed EudoraHTMLParser's initialization logic so that we always get a clean self.cids.

--- EudoraHTMLParser.py
+++ EudoraHTMLParser.py
@@ -4,21 +4,20 @@ from HTMLParser import HTMLParser, HTMLParseError
 
 class EudoraHTMLParser(HTMLParser):
 
-	cids = []
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.cids = []
 
 	def handle_starttag(self, tag, attrs):
 		if tag == "img":
 			for k, v in attrs:
 				if k == 'src' and v.startswith('cid:'):
-					print v[4:]
 					self.cids.append(v[4:])
 					
 	def handle_endtag(self, tag):
 		pass
 
-	def get_reset_cids(self):
-		retr = self.cids
-		self.cids = []
+	def get_cids(self):
 		return self.cids
 
 
